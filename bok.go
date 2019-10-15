b41f3b4f1f44b6e5914d91353e81bd2f528e25fa
Hauke Stieler <mail@hauke-stieler.de>
2019-10-16 00:54:09 +0200

    Add first repl and ask for data to add

--- bok.go
+++ bok.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"os"
 
 	"github.com/hauke96/kingpin"
@@ -54,5 +56,56 @@ func main() {
 	switch cmd {
 	case appAdd.FullCommand():
 		sigolo.Debug("Use account file '%s'", *addFile)
+		// TODO add entry
 	}
+
+	RunRepl()
+}
+
+func RunRepl() {
+	var cmd string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("> ")
+	for scanner.Scan() {
+		cmd = scanner.Text()
+
+		if cmd == "q" || cmd == "quit" || cmd == "exit" {
+			break
+		}
+		switch cmd {
+		case "add", "a":
+			replAddEntry(scanner)
+		}
+
+		fmt.Print("> ")
+	}
+
+	if err := scanner.Err(); err != nil {
+		sigolo.Fatal("Error reading standard input: ", err.Error())
+	}
+}
+
+func replAddEntry(scanner *bufio.Scanner) {
+	var date string
+	var amount string
+	var description string
+
+	sigolo.Info("Add new entry:")
+
+	fmt.Print("  Date: ")
+	scanner.Scan()
+	date = scanner.Text()
+
+	fmt.Print("  Amount: ")
+	scanner.Scan()
+	amount = scanner.Text()
+
+	fmt.Print("  Description: ")
+	scanner.Scan()
+	description = scanner.Text()
+
+	// TODO addEntry(date, amount, description)
+
+	sigolo.Debug("Added entry [%s, %s, '%s']", date, amount, description)
 }
